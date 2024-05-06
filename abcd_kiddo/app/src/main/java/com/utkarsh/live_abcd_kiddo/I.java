2fa94f1a28b66f81530eae703dfe7eab8cb7362e
utkarshsingh47 <utkarshsingh9548@gmail.com>
2024-05-06 10:46:36 +0530

    this is a kids game which helps small children learn a-z

---
+++ abcd_kiddo/app/src/main/java/com/utkarsh/live_abcd_kiddo/I.java
@@ -0,0 +1,51 @@
+package com.utkarsh.live_abcd_kiddo;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class I extends AppCompatActivity {
+    Button btn_next;
+    ImageView iv_play;
+    MediaPlayer mediaPlayer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_i);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        btn_next=findViewById(R.id.btn_next);
+        iv_play=findViewById(R.id.iv_play);
+playmusic();
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(I.this,J.class));
+            }
+        });
+
+        iv_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+playmusic();
+            }
+        });
+    }
+    private void playmusic(){
+        mediaPlayer= MediaPlayer.create(getApplicationContext(),R.raw.i);
+        mediaPlayer.start();
+    }
+}
